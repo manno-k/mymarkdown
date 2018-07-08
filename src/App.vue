92e0bee1b20779be403cd3ce330a2d96c90616b3
manno-k <art.kuromi@gmail.com>
2018-07-08 20:35:21 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Home v-if="!isLogin"></Home>
+    <Editor v-if="isLogin"></Editor>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+import Editor from './components/Editor'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      isLogin: false
+    }
+  },
+  components: {
+    'Home': Home,
+    'Editor': Editor
+  }
+}
+</script>
